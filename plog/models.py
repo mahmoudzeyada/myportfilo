650bfe8b611c3b9b31d8be16417d362ef2838ac7
Mahmoud Zeyada <mahmoudzeyada@192.168.1.2>
2018-09-15 13:09:22 +0200

    adding plog app

--- plog/models.py
+++ plog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class plog(models.Model):
+    title=models.CharField(max_length=200)
+    pub_date=models.DateField(auto_now=True)
+    body=models.CharField(max_length=500)
+    image = models.ImageField(upload_to="images/")
