8cea5a1ba76cd42300cbb31a110de0490bd8d9e3
Mahmoud Zeyada <mahmoudzeyada@192.168.1.2>
2018-09-15 15:43:37 +0200

    adding botstrap for jops app

--- protofilo/urls.py
+++ protofilo/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+import jops.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",jops.views.home)
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
